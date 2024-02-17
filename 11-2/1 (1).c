7ea3ae0bc4eab33fc75a2ef6fb6000986de70a2b
Parth Pipaliya <158568547+parthpipaliya6@users.noreply.github.com>
2024-02-17 10:25:50 +0530

    c-lunguage

---
+++ 11-2/1 (1).c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	int s,i,j;
+	//clrscr();
+	
+	for(i=1;i<=5;i++)
+	{
+		for(s=5;s>i;s--)
+		{
+			printf("  ");	
+		}
+		for(j=i;j>=1;j--)
+		{
+			printf("%d ",j);
+		}
+		printf("\n");
+	}
+	getch();
+}
