915b701932634b178c378768ec73ccd38eebe124
Parth Pipaliya <158568547+parthpipaliya6@users.noreply.github.com>
2024-04-07 17:58:26 +0530

    1.c

--- PR-9/1.c
+++ PR-9/1.c
@@ -1,36 +1,20 @@
-#include <stdio.h>
+#include<stdio.h>
+#define p printf
+#define s scanf
 
-int main() {
-    FILE *even_file, *odd_file;
-    int st = 50, end = 70;
-
-    even_file = fopen("even_file.txt", "w");
-    odd_file = fopen("odd_file.txt", "w");
-
-    if (even_file == NULL || odd_file == NULL) 
-	{
-        printf("Error opening files.\n");
-        return 1;
-    }
-
-    for (int num = st; num <= end; num++) 
-	{
-        if (num % 2 == 0) 
-		{
-            fprintf(even_file, "%d, ", num);
-        } else 
-		{
-            fprintf(odd_file, "%d, ", num);
-        }
-	}	
-    fclose(even_file);
-    fclose(odd_file);
-
-    printf("Even numbers from even_file.txt:\n");
-    printf("50, 52, 54, 56, 58, 60, 62, 64, 66, 68, 70\n\n");
-
-    printf("Odd numbers from odd_file.txt:\n");
-    printf("51, 53, 55, 57, 59, 61, 63, 65, 67, 69\n");
+int main()
+{
+    FILE *fp1, *fp2;
+    int i;
     
-
+    fp1 = fopen("EVEN-NUM.txt","w");
+    fp2 = fopen("ODD-NUM.txt","w");
+    for (i = 50; i <= 70; i++)
+    {
+        (i%2==0)
+            ?fprintf(fp1,"%d, ",i)
+            :fprintf(fp2,"%d, ",i);
+    }
+    
+    return 0;
 }
