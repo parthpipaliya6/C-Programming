abc592ccd9ca7e8a6c23b0338bd3b9021681a318
Parth Pipaliya <158568547+parthpipaliya6@users.noreply.github.com>
2024-04-12 15:05:58 +0530

    6.c

--- FINAL EXAM/6.c
+++ FINAL EXAM/6.c
@@ -1,45 +1,42 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 
 struct student {
-	int id;
-	char name[20];
-	float per;
+    int id;
+    char name[20];
+    float per;
 };
 
-void main ()
-{
-	FILE *fp;
-	fp = fopen("deta.txt","w");
-	
-	int n;
-	printf("Enter for student :");
-	scanf("%d",&n);
-	
-	 struct student s1[n]; 
-	 int i ;
-	 for (i=0;i<n;i++)
-	 {
-	 	printf("Student id :");
-	 	scanf ("%d",&s1[i].id);
-	 	fflush(stdin);
-	 	printf("Student name  :");
-	 	scanf ("%[^\n]",&s1[i].id);
-		printf("Student per :");
-	 	scanf ("%f",&s1[i].id);
-	 }
-	 
-	printf("\n\n--:OUTPUT:--\n\n");
-	
-	for(i=0;i < n; i++)
-	{
-		if(fp==NULL)
-		printf("file is note avelebal...");
-	}
-	else 
-	{
-		printf("file open in succsesfully...");
-	}
-	fclose(fp);
+int main() {
+    FILE *fp;
+    fp = fopen("deta.txt", "w");
 
+    int n;
+    printf("Enter the number of students: ");
+    scanf("%d", &n);
+
+    struct student s1[n];
+    int i;
+   	if (fp !=NULL)
+   	{
+		for (i = 0; i < n; i++) 
+		{
+			printf("Student ID: ");
+			scanf("%d", &s1[i].id);
+			fflush(stdin);
+			printf("Student name: ");
+			scanf("%[^\n]", s1[i].name);
+			printf("Student percentage: ");
+			scanf("%f", &s1[i].per);
+		}
+
+			printf("\n-- OUTPUT --\n\n");
+		for (i = 0; i < n; i++) 
+		{
+			printf("Student ID: %d\n", s1[i].id);
+			printf("Student name: %s\n", s1[i].name);
+			printf("Student percentage: %.2f\n\n", s1[i].per);
+		}
+	}
+ 
 }
