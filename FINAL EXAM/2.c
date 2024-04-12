9be70e865a7d772c4a800fa8e48de40891705f69
Parth Pipaliya <158568547+parthpipaliya6@users.noreply.github.com>
2024-04-12 14:51:34 +0530

    2.c

--- FINAL EXAM/2.c
+++ FINAL EXAM/2.c
@@ -3,41 +3,43 @@
 main()
 {
 	int r,c,i,j;
-	printf("Enter row :");
+	
+	printf("Enter Row : ");
 	scanf("%d",&r);
-	printf("Enter column :");
+	printf("Enter Column : ");
 	scanf("%d",&c);
 	int a[r][c];
 	
-	for (i=0;i<r;i++)
+	for(i=0;i<r;i++)
 	{
-		for (j=0;j<c;j++)
+		for(j=0;j<c;j++)
 		{
-			printf("a[%d][%d] :",i,j);
+			printf("a[%d][%d] : ",i,j);
 			scanf("%d",&a[i][j]);
 		}
 	}
-	printf("\n");
 	
-	for (i=0;i<r;i++)
+	printf("\n");
+	for(i=0;i<r;i++)
 	{
-		for (j=0;j<c;j++)
+		for(j=0;j<c;j++)
 		{
 			printf("%d ",a[i][j]);
 		}
 		printf("\n");
 	}
-	int n=a[2][2];
 	
-//	for (i=0;i<r;i++)
-//	{
-//		for (j=0;j<c;j++)
-//		{
-//			printf("%d ",a[i][j]);
-//		}
-//		printf("\n");
-//	}
+	int n = a[0][0];
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			if(a[i][j]>n)
+			{
+				n = a[i][j];	
+			}
+		}
+	}
 	
-	printf("\n largest element : %d",n);
-	
+	printf("\nLargest Element : %d",n);
 }
